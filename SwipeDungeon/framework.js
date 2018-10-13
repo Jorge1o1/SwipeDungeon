dc3c98e5b2eb9a670a3969fef29926f2c3d15bb8
maldos21 <40219608+maldos21@users.noreply.github.com>
2018-10-13 14:30:21 -0400

    Swipe
    No moving

--- SwipeDungeon/framework.js
+++ SwipeDungeon/framework.js
@@ -1,9 +1,9 @@
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 
-document.getElementById("myCanvas").addEventListener("mousedown", function(event) {handleInput(0, 0, event);});
+document.getElementById("myCanvas").addEventListener("ondrag", function(event) {handleInput(0, 0, event);});
 document.getElementById("myCanvas").addEventListener("touchmove", function(event) {handleInput(1, 0, event);});
-document.getElementById("myCanvas").addEventListener("mouseup", function(event) {handleInput(0, 1, event);});
+document.getElementById("myCanvas").addEventListener("ondrag", function(event) {handleInput(0, 1, event);});
 document.getElementById("myCanvas").addEventListener("touchend", function(event) {handleInput(0, 1, event);});
 
 var FRAMEWORK_CONSTANTS = {
@@ -15,6 +15,7 @@ var FRAMEWORK_CONSTANTS = {
 //FOR REFERENCE:
 var game = {
 	tick: 0, //do not modify
+	touchPoints:[],
 	constants:{
 		enemySpawnRate: 50,
 		friction: 0.998
@@ -77,28 +78,20 @@ function handleInput(type, action, e){ //function optimized for portability; run
 		//handle computer input
 		if(action == 0){
 			//handle mousemove
-			if(game.mouse.start.x == -1 && game.mouse.start.y == -1){
-				game.mouse.start = {x: e.clientX, y: e.clientY};
-			}
+			console.log("DRAGGING");
+			game.touchPoints.push({x: e.clientX, y: e.clientY});
 		}else{
 			//handle mouseup
-			var swipe = {x: (e.clientX - game.mouse.start.x), y: (e.clientY - game.mouse.start.y)};
-			game.mouse.start = {x: -1, y: -1};
-			receiveInput(game.player, swipe);
+			receiveInput();
 		}
 	}else{
 		//handle mobile input - NOTE: THIS HAS NOT BEEN UNIT TESTED YET
 		if(action == 0){
-			//handle touchmove
-			if(game.mouse.start.x == -1 && game.mouse.start.y == -1){
-				game.mouse.start = {x: e.touches[0].pageX, y: e.touches[0].pageY};
-			}
-
+			//handle mousemove
+			game.touchPoints.push({x: e.touches[0].pageX, y: e.touches[0].pageY});
 		}else{
-			//handle touchup
-			var swipe = {x: (e.touches[0].pageX - game.mouse.start.x), y: (e.touches[0].pageY - game.mouse.start.y)};
-			game.mouse.start = {x: -1, y: -1};
-			receiveInput(game.player, swipe);
+			//handle mouseup
+			receiveInput();
 		}
 	}
 }
